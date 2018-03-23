e12498a09b00ad73666f7716df222e5bd69bd2c3
Polar <jvasconsuelom@gmail.com>
2018-03-23 07:55:54 -0500

    changes

--- digital_challenge/p_settings.py
+++ digital_challenge/p_settings.py
@@ -4,8 +4,18 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-STATIC_ROOT = '/home/polar/webapps/recursos/digital_challenge/static/'
+STATIC_ROOT = '/home/polar/webapps/recursos/digital/static/'
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = '/home/polar/webapps/recursos/digital_challenge/media/'
+MEDIA_ROOT = '/home/polar/webapps/recursos/digital/media/'
 MEDIA_URL = '/media/'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'digital',
+        'USER': 'digital',
+        'PASSWORD': 'Ericelis21',
+        'HOST': 'localhost'
+    }
+}
--- digital_challenge/urls.py
+++ digital_challenge/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', admin.site.urls),
     path('common/', include('apps.comun.urls', namespace='common')),
     path('api-auth/', include('rest_framework.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
